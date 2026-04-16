394432c4a64bf26971a170966d6aae7fc9c72176
Jhonata Freitas <86995208+ALVlN@users.noreply.github.com>
2026-04-16 20:30:15 -0300

    SoluçãoTask1050

---
+++ Lista01/1050-DDD/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if(input==61){
+            System.out.println("Brasilia");
+        } else if (input==71){
+            System.out.println("Salvador");
+        } else if (input==11){
+            System.out.println("Sao Paulo");
+        } else if (input==21){
+            System.out.println("Rio de Janeiro");
+        } else if (input==32){
+            System.out.println("Juiz de Fora");
+        } else if (input==19){
+            System.out.println("Campinas");
+        } else if (input==27){
+            System.out.println("Vitoria");
+        } else if (input==31){
+            System.out.println("Belo Horizonte");
+        } else{
+            System.out.println("DDD nao cadastrado");
+        }
+    }
+
+}

b8fd14eaf7b0c3ac8230a3647ae5336f5c63943b
Jhonata Freitas <86995208+ALVlN@users.noreply.github.com>
2026-04-17 05:03:27 -0300

    SoluçãoTask1096

---
+++ Lista01/1096-SequenciaIj2/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        for(int i = 1; i <= 9; i += 2){
+                System.out.printf("I=%d J=7%n", i);
+                System.out.printf("I=%d J=6%n", i);
+                System.out.printf("I=%d J=5%n", i);
+        }
+    }
+
+}
